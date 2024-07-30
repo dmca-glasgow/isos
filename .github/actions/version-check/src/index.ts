07f86681b2d658984aa2f7d3e89d86d73ad97815
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2024-07-30 16:23:12 +0100

    debug

---
+++ .github/actions/version-check/src/index.ts
@@ -0,0 +1,39 @@
+import { setOutput, setFailed } from '@actions/core';
+import { context } from '@actions/github';
+import { exec } from '@actions/exec';
+import { mkdirP } from '@actions/io';
+import { readFile } from 'fs/promises';
+
+run();
+
+async function run() {
+  await exec('ls', ['-la', '/github/workspace']);
+  // try {
+  //   const { owner, repo } = context.repo;
+  //   const url = `https://github.com/${owner}/${repo}.git`
+
+  //   await exec('ls', ['-la', '/github/workspace']);
+
+  //   await mkdirP('workspace');
+  //   await exec('git', ['clone', url, 'workspace'], { silent: true });
+
+  //   const newVersion = await getVersion();
+  //   // console.log(`New version: ${newVersion}`);
+
+  //   await exec('git', ['checkout', 'HEAD^'], { cwd: 'workspace', silent: true });
+  //   const oldVersion = await getVersion();
+  //   // console.log(`Old version: ${oldVersion}`);
+
+  //   setOutput('has-version-change', `${newVersion !== oldVersion}`)
+
+  // } catch (error) {
+  //   setFailed(error as Error);
+  // }
+}
+
+async function getVersion(): Promise<string> {
+  const filePath = './workspace/src-tauri/tauri.conf.json'
+  const contents = await readFile(filePath, 'utf-8')
+  const json = JSON.parse(contents)
+  return json.package.version;
+}
