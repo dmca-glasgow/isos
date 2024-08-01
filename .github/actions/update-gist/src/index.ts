5690acd82707b449bdca002c24b5d0f539077d2d
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2024-08-01 09:18:17 +0100

    debug

---
+++ .github/actions/update-gist/src/index.ts
@@ -0,0 +1,43 @@
+import { setOutput, setFailed } from '@actions/core';
+import { context } from '@actions/github';
+import { exec } from '@actions/exec';
+import { mkdirP } from '@actions/io';
+import { readFile } from 'fs/promises';
+
+run();
+
+const gistId = '12a09637fb047aa519cc2ea5fd662a8c'
+
+async function run() {
+  try {
+    await exec('ls', ['-la', './gist'])
+
+    // Download the updater json file
+    // gh release download app-v0.0.2 --pattern 'latest.json' --output -
+
+    // Publish a release that was previously a draft
+    // gh release edit app-v0.0.2 --draft=false
+
+    // overwriting file
+    // gh gist edit gist-id -f gist-filename local-filename
+
+  } catch (error) {
+    setFailed(error as Error);
+  }
+}
+
+async function getPreviousReleaseVersions() {
+  let output = ''
+
+  await exec('gh', ['release', 'list', '--json', 'name'], {
+    listeners: {
+      stdout: (data) => {
+        output += data.toString()
+      }
+    }
+  });
+
+  const json = JSON.parse(output) as Array<{name: string}>
+
+  return json.map(o => o.name)
+}
--- .github/actions/version-check/src/index.ts
+++ .github/actions/version-check/src/index.ts
@@ -7,45 +7,35 @@ import { readFile } from 'fs/promises';
 run();
 
 async function run() {
-  const newVersion = await getVersion();
-  console.log(`New version: ${newVersion}`);
+  try {
+    await mkdirP('workspace');
 
-  let cmdOutput = ''
-  const options = {
-    listeners: {
-      stdout: (data) => {
-        cmdOutput += data.toString()
+    const { owner, repo } = context.repo;
+    const url = `https://github.com/${owner}/${repo}.git`
+
+    await exec('git', ['clone', url, 'workspace'], { silent: true });
+    const newVersion = await getVersion();
+    console.log(`New version: ${newVersion}`);
+
+    await exec('git', ['checkout', 'HEAD^'], { cwd: 'workspace', silent: true });
+    const oldVersion = await getVersion();
+    console.log(`Old version: ${oldVersion}`);
+
+    if (newVersion !== oldVersion) {
+      const previousVersions = await getPreviousReleaseVersions()
+      console.log(`Previous versions: ${previousVersions}`);
+
+      if (previousVersions.includes(newVersion)) {
+        setFailed(`Error: version ${newVersion} has been released previously`);
+        return
       }
+
+      setOutput('has-version-change', `true`)
     }
+
+  } catch (error) {
+    setFailed(error as Error);
   }
-
-  await exec('gh', ['release', 'list', '--json', 'name']);
-
-  console.log('cmdOutput:', cmdOutput)
-
-  // git blame -L '/version/',+1 -- src-tauri/tauri.conf.json
-  // git log -L 10,11:src-tauri/tauri.conf.json
-  // gh release list --json name
-
-  // try {
-  //   const { owner, repo } = context.repo;
-  //   const url = `https://github.com/${owner}/${repo}.git`
-
-  //   await mkdirP('workspace');
-  //   await exec('git', ['clone', url, 'workspace'], { silent: true });
-
-  //   const newVersion = await getVersion();
-  //   // console.log(`New version: ${newVersion}`);
-
-  //   await exec('git', ['checkout', 'HEAD^'], { cwd: 'workspace', silent: true });
-  //   const oldVersion = await getVersion();
-  //   // console.log(`Old version: ${oldVersion}`);
-
-  //   setOutput('has-version-change', `${newVersion !== oldVersion}`)
-
-  // } catch (error) {
-  //   setFailed(error as Error);
-  // }
 }
 
 async function getVersion(): Promise<string> {
@@ -53,4 +43,20 @@ async function getVersion(): Promise<string> {
   const contents = await readFile(filePath, 'utf-8')
   const json = JSON.parse(contents)
   return json.package.version;
+}
+
+async function getPreviousReleaseVersions() {
+  let output = ''
+
+  await exec('gh', ['release', 'list', '--json', 'name'], {
+    listeners: {
+      stdout: (data) => {
+        output += data.toString()
+      }
+    }
+  });
+
+  const json = JSON.parse(output) as Array<{name: string}>
+
+  return json.map(o => o.name)
 }
